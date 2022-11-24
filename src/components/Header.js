b6bc1d510265df042211b719ad24d7ab382b8a87
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-11-24 17:37:30 +0300

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="header-div">
+            <h1>Welcome Where you can plan yourself</h1>
+        </div>
+    )
+}
+
+export default Header;
